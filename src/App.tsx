a294f4ab3bd1db6628074d72992248c9a99ec73f
VincentLabor <vlabor95@gmail.com>
2021-03-12 17:58:40 -0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
